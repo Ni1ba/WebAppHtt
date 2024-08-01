f2c5d21e052a29dce8fffe85563accdf5b10efc8
Ni1ba <danila.baldin@mail.ru>
2024-08-01 17:36:26 +0300

    Работает

--- HTTWebApplication/Program.cs
+++ HTTWebApplication/Program.cs
@@ -3,17 +3,19 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using HTTWebApplication.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
@@ -23,17 +25,18 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
- 
+app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapControllers();  
 
-app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
         name: "default",
-        pattern: "{controller=ProductsController}/{action=Index}/{id?}");
+        pattern: "{controller=Products}/{action=Index}/{id?}");
 });
+
 app.Run();
